f2299dfd16c901f86be255e5f1d27c6c01520eee
Hascine <thehascine@gmail.com>
2024-05-23 11:05:09 +0700

    styling homepage

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -31,10 +31,12 @@ const HomePage = () => {
 
     socket.connect();
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((currentStream) => {
-      setStream(currentStream);
-      selfVideoRef.current.srcObject = currentStream;
-    });
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((currentStream) => {
+        setStream(currentStream);
+        selfVideoRef.current.srcObject = currentStream;
+      });
 
     socket.on('self', ({ self }) => {
       setSelf(self);
@@ -59,54 +61,85 @@ const HomePage = () => {
       socket.off('call:accepted');
       socket.off('call:ended');
     };
-  }, [selfVideoRef, remoteVideoRef, setStream, setSelf, setIsCallEnded, setCall]);
+  }, [
+    selfVideoRef,
+    remoteVideoRef,
+    setStream,
+    setSelf,
+    setIsCallEnded,
+    setCall,
+  ]);
 
   return (
-    <div>
-      {/* {stream && ( */}
-      <div>
-        <p>Name: {self?.name}</p>
-        <p>ID: {self?.socketId}</p>
-        {/* {selfVideoRef.current.srcObject ? ( */}
-        <video ref={selfVideoRef} autoPlay muted />
-        {/* ) : (
+    <>
+      <section className="flex flex-col justify-center bg-slate-900 min-h-screen p-5">
+        <div
+          className={`bg-slate-100 grid ${
+            call ? 'grid-cols-2' : 'grid-cols-1'
+          } justify-items-center rounded-md`}
+        >
+          {/* {stream && ( */}
+          <div>
+            <p>Name: {self?.name}</p>
+            <p>ID: {self?.socketId}</p>
+            {/* {selfVideoRef.current.srcObject ? ( */}
+            <video ref={selfVideoRef} autoPlay muted />
+            {/* ) : (
           <img src="https://i.pravatar.cc/300" alt="" />
         )} */}
-        <button onClick={toggleVideo}>Toggle Video</button>
-        <button onClick={toggleAudio}>Toggle Audio</button>
-      </div>
-      {/* )} */}
+            <button onClick={toggleVideo}>Toggle Video</button>
+            <button onClick={toggleAudio}>Toggle Audio</button>
+          </div>
+          {/* )} */}
 
-      {isCallAccepted && !isCallEnded && (
-        <div>
-          <p>Name: {call.remote.name}</p>
-          <p>ID: {call.remote.socketId}</p>
-          {/* {remoteVideoRef.current.srcObject ? ( */}
-          <video ref={remoteVideoRef} autoPlay />
-          {/* ) : (
+          {isCallAccepted && !isCallEnded && (
+            <div>
+              <p>Name: {call.remote.name}</p>
+              <p>ID: {call.remote.socketId}</p>
+              {/* {remoteVideoRef.current.srcObject ? ( */}
+              <video ref={remoteVideoRef} autoPlay />
+              {/* ) : (
             <img src="https://i.pravatar.cc/300" alt="" />
           )} */}
+            </div>
+          )}
         </div>
-      )}
+        <div className="flex justify-center bottom-10">
+          {isCallAccepted && !isCallEnded ? (
+            <div>
+              <button className="btn" onClick={endCall}>
+                End Call
+              </button>
+            </div>
+          ) : (
+            <div className="flex flex-col justify-center">
+              <div>
+                <input
+                  type="text"
+                  value={remoteId}
+                  onChange={(e) => setRemoteId(e.target.value)}
+                />
+              </div>
+              <div>
+                <button
+                  className="btn justify-center"
+                  onClick={() => startCall(remoteId)}
+                >
+                  Call
+                </button>
+              </div>
+            </div>
+          )}
 
-      {isCallAccepted && !isCallEnded ? (
-        <div>
-          <button onClick={endCall}>End Call</button>
+          {call && !isCallAccepted && (
+            <div>
+              <p>{call.remote.name} is calling...</p>
+              <button onClick={acceptCall}>Accept</button>
+            </div>
+          )}
         </div>
-      ) : (
-        <div>
-          <input type="text" value={remoteId} onChange={(e) => setRemoteId(e.target.value)} />
-          <button onClick={() => startCall(remoteId)}>Call</button>
-        </div>
-      )}
-
-      {call && !isCallAccepted && (
-        <div>
-          <p>{call.remote.name} is calling...</p>
-          <button onClick={acceptCall}>Accept</button>
-        </div>
-      )}
-    </div>
+      </section>
+    </>
   );
 };
 
