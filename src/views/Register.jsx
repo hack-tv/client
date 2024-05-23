2e3c6f73ce49735ff7ffa4a3694a717edcb25935
pristiwanakbars <pristiwanakbarsubery@gmail.com>
2024-05-23 22:42:52 +0700

    loginpage, register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import { GoogleLogin } from "@react-oauth/google";
-import { useNavigate, Link } from "react-router-dom";
-import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "../lib/axios";
-
+import { toast } from "react-toastify";
 import {
   Card,
   Input,
@@ -12,67 +10,64 @@ import {
   Typography,
 } from "@material-tailwind/react";
 
-const LoginPage = () => {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const navigate = useNavigate();
   const url = "http://localhost:3000";
 
-  const handleDefaultLogin = async (e) => {
+  async function handleRegister(e) {
     e.preventDefault();
-
     try {
-      const { data } = await axios.post(`${url}/auth/login`, {
-        email,
-        password,
-      });
+      const registered = { name, email, password };
+      const { data } = await axios.post(`${url}/auth/register`, registered);
 
-      localStorage.setItem("token", data.token);
-      toast.success("Successfully logged in");
-      navigate("/");
-    } catch (error) {
-      console.log(error, "<<erorrrrr");
-      toast.error(error.response.data.message);
-    }
-  };
-
-  const handleGoogleLogin = async (response) => {
-    try {
-      const { data } = await axios.get("/auth/google", {
-        headers: {
-          token: response.credential,
-        },
-      });
-
-      localStorage.setItem("token", data.access_token);
-      toast.success("Successfully logged in");
-      navigate("/");
+      toast.success("Successfully register");
+      navigate("/login");
     } catch (error) {
       toast.error(error.response.data.message);
     }
-  };
+  }
 
   return (
     <>
-      <section className="m-8 flex gap-4">
-        <div className="w-full mt-0">
+      <section className="m-8 flex">
+        <div className="w-full lg:w-3/5 flex flex-col items-center justify-center">
           <div className="text-center">
             <Typography variant="h2" className="font-bold mb-4">
-              Welcome to HackTV!
+              Join HackTV
             </Typography>
             <Typography
               variant="paragraph"
               color="blue-gray"
               className="text-lg font-normal"
             >
-              Enter your email and password to Sign In.
+              Enter your name email and password to join HackTV
             </Typography>
           </div>
           <form
             className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2"
-            onSubmit={handleDefaultLogin}
+            onSubmit={handleRegister}
           >
             <div className="mb-1 flex flex-col gap-6">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="-mb-3 font-medium"
+              >
+                Your name
+              </Typography>
+              <Input
+                size="lg"
+                placeholder="your name"
+                onChange={(e) => setName(e.target.value)}
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+              />
+
               <Typography
                 variant="small"
                 color="blue-gray"
@@ -82,7 +77,7 @@ const LoginPage = () => {
               </Typography>
               <Input
                 size="lg"
-                placeholder="yourmail@mail.com"
+                placeholder="name@mail.com"
                 onChange={(e) => setEmail(e.target.value)}
                 className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
                 labelProps={{
@@ -109,26 +104,16 @@ const LoginPage = () => {
             </div>
 
             <Button type="submit" className="mt-6" fullWidth>
-              Sign In
+              Register Now
             </Button>
 
-            <div className="space-y-4 mt-8">
-              <Button
-                size="lg"
-                color="white"
-                className="flex items-center gap-2 justify-center shadow-md"
-                fullWidth
-              >
-                <GoogleLogin onSuccess={handleGoogleLogin} />
-              </Button>
-            </div>
             <Typography
               variant="paragraph"
               className="text-center text-blue-gray-500 font-medium mt-4"
             >
-              Not registered?
-              <Link to="/auth/register" className="text-gray-900 ml-1">
-                Create account
+              Already have an account?
+              <Link to="/login" className="text-gray-900 ml-1">
+                Sign in
               </Link>
             </Typography>
           </form>
@@ -136,6 +121,4 @@ const LoginPage = () => {
       </section>
     </>
   );
-};
-
-export default LoginPage;
+}
